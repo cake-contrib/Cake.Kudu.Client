b229f807ce29da3374d33838c5b5ea1a19ce4a8f
Mattias Karlsson <matkar@wcom.se>
2018-02-16 11:03:17 +0100

    Added AppSettings xmldoc examples

--- src/Cake.Kudu.Client/Extensions/KuduClientSettingsExtensions.cs
+++ src/Cake.Kudu.Client/Extensions/KuduClientSettingsExtensions.cs
@@ -15,6 +15,30 @@ namespace Cake.Kudu.Client.Extensions
         /// </summary>
         /// <param name="client">The Kudu client.</param>
         /// <returns>The appservice settings.</returns>
+        /// <example>
+        /// <code>
+        /// #addin nuget:?package=Cake.Kudu.Client
+        ///
+        /// string  baseUri     = EnvironmentVariable("KUDU_CLIENT_BASEURI"),
+        ///         userName    = EnvironmentVariable("KUDU_CLIENT_USERNAME"),
+        ///         password    = EnvironmentVariable("KUDU_CLIENT_PASSWORD");
+        ///
+        /// IKuduClient kuduClient = KuduClient(
+        ///     baseUri,
+        ///     userName,
+        ///     password);
+        ///
+        /// ReadOnlyDictionary&lt;string, string&gt; settings = kuduClient.SettingsGet();
+        ///
+        /// foreach(var setting in settings)
+        /// {
+        ///     Information(
+        ///         "{0}={1}",
+        ///         setting.Key,
+        ///         setting.Value);
+        /// }
+        /// </code>
+        /// </example>
         public static IReadOnlyDictionary<string, string> SettingsGet(
             this IKuduClient client)
         {
@@ -32,6 +56,29 @@ namespace Cake.Kudu.Client.Extensions
         /// </summary>
         /// <param name="client">The Kudu client.</param>
         /// <param name="settings">The settings to set.</param>
+        /// <example>
+        /// <code>
+        /// #addin nuget:?package=Cake.Kudu.Client
+        ///
+        /// string  baseUri     = EnvironmentVariable("KUDU_CLIENT_BASEURI"),
+        ///         userName    = EnvironmentVariable("KUDU_CLIENT_USERNAME"),
+        ///         password    = EnvironmentVariable("KUDU_CLIENT_PASSWORD");
+        ///
+        /// IKuduClient kuduClient = KuduClient(
+        ///     baseUri,
+        ///     userName,
+        ///     password);
+        ///
+        /// var newSettings = new Dictionary&lt;string, string&gt;
+        ///     {
+        ///         { "FOO", "Bar" },
+        ///         { "JOHN", "Doe" }
+        ///     };
+        ///
+        /// kuduClient.SettingsSet(
+        ///     newSettings);
+        /// </code>
+        /// </example>
         public static void SettingsSet(
             this IKuduClient client,
             IReadOnlyDictionary<string, string> settings)
@@ -56,6 +103,22 @@ namespace Cake.Kudu.Client.Extensions
         /// </summary>
         /// <param name="client">The Kudu client.</param>
         /// <param name="key">The key of settings to delete.</param>
+        /// <example>
+        /// <code>
+        /// #addin nuget:?package=Cake.Kudu.Client
+        ///
+        /// string  baseUri     = EnvironmentVariable("KUDU_CLIENT_BASEURI"),
+        ///         userName    = EnvironmentVariable("KUDU_CLIENT_USERNAME"),
+        ///         password    = EnvironmentVariable("KUDU_CLIENT_PASSWORD");
+        ///
+        /// IKuduClient kuduClient = KuduClient(
+        ///     baseUri,
+        ///     userName,
+        ///     password);
+        ///
+        /// kuduClient.SettingsDelete("FOO");
+        /// </code>
+        /// </example>
         public static void SettingsDelete(
             this IKuduClient client,
             string key)
